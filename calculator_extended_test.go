d327a8d9cf7382764998542e5dc3b90713582e11
Abhinav Sinha <37282098+zeborg@users.noreply.github.com>
2022-03-18 01:28:03 +0530

    Update calculator_extended_test.go

--- calculator_extended_test.go
+++ calculator_extended_test.go
@@ -4,6 +4,6 @@ import "testing"
 
 func TestDivide(t *testing.T) {
 	if got, want := Divide(4, 2), 2; got != want {
-		t.Errorf("multiply method produced wrong result. expected: %d, got: %d", want, got)
+		t.Errorf("multiply method produced wrong result. expected: %d, got: %d ", want, got)
 	}
 }
